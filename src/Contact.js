42d02523cb3be06d1f003ee8e1772e53e222f00d
yodit melile <yoditmelile@gmail.com>
2024-01-07 19:19:53 +0300

    today

--- src/Contact.js
+++ src/Contact.js
@@ -1,53 +1,55 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+   
+    faPhone,
+    faGlobe,
+    faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 import { FaThumbsUp, FaPhoneSquareAlt } from "react-icons/fa";
-import { IoIosRocket } from "react-icons/io";
-import { MdOutlineSocialDistance } from "react-icons/md";
 
 const Contact = () => {
-  return (
-    <Container fluid>
-      <Row className="justify-content-center h-70">
-        <Col md={3} className="d-flex align-items-center">
-          <div className="bg-white p-4 text-center" style={{ border: '2px solid #C0A142' }}>
-            <FaThumbsUp className="icons" />
-            <h3>100% Satisfaction Guaranteed</h3>
-            <p>High quality, Accessibility, Africanism, Convenience and Consistency </p>
-          </div>
-        </Col>
-        <Col md={3}>
-          <Row className="h-100">
-            <Col md={12} className="d-flex align-items-center justify-content-center">
-              <div className="bg-white p-4 text-center" style={{ border: '2px solid #C0A142' }}>
-                <FaPhoneSquareAlt className="icons" />
-                <h3>Contact Us</h3>
-                <p>+2519087654/+2519876543</p>
-              </div>
-            </Col>
-            <Col md={12} className="d-flex align-items-center justify-content-center">
-              <div className="bg-white p-4 text-center" style={{ border: '2px solid  #C0A142' }}>
-                <IoIosRocket className="icons" />
-                <h3> Delivery</h3>
-                <h6>free delivery sgsegedthxfgt t</h6>
-              </div>
-            </Col>
-          </Row>
-        </Col>
-        <Col md={3} className="d-flex align-items-center">
-          <div className="bg-white p-4 text-center" style={{ border: '2px solid #C0A142' }}>
-            <MdOutlineSocialDistance className="icons" />
-            <h3>Social media</h3>
-            <h6>Email: info@Kibirtgmail</h6>
-            <h6>Instagram: kibirt</h6>
-            <h6>Telegram: kibirt</h6>
-            <h6>Facebook: kibirt</h6>
-            <h6>X: kibirt</h6>
+    const rectangles = [
+        {
+            icon: faThumbsUp,
+            label: "100% Satisfaction Guaranteed",
+            label2: "High quality, Accessibility, Africanism, Convenience and Consistency",
+        },
+        {
+            icon: faPhone,
+            label: "Contact Us",
+            label2: "+2519087654/+2519876543",
+        },
+        {
+            icon: faGlobe,
+            label: "Social media",
+            label2: "Email: info@Kibirtgmail ",
+            label3: "Instagram: kibirt",
            
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+        },
+    ];
+
+    return (
+        <div className="textAdvertismentContainer">
+            {rectangles.map((item, index) => (
+                <div key={index} className="textAdvertismentRectangle">
+                    <div className="textAdvertismentContent">
+                        <div className="textAdvertismentIcon">
+                            <FontAwesomeIcon
+                                icon={item.icon}
+                                className="catIcon"
+                            />
+                        </div>
+                        <div className="textAdvertismentLabel">
+                            <h2>{item.label}</h2>
+                            <h5>{item.label2}</h5>
+                            <h5>{item.label3}</h5>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Contact;
---
+++ src/MIssion.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./App.css";
+
+export default function Mission() {
+  return (
+
+  
+          <section id="mission" class="mission" >
+      <div class="container" data-aos="fade-up">
+        <div class="row align-items-center">
+          <div class="col-md-6">
+            
+          </div>
+    
+          <div class="col-md-6 mt-5 mt-md-0 ps-md-5 text-white content">
+            <h2 class="section_title">Mission</h2>
+            <p id="mission-firstParagraph" class="mt-3">
+             
+            To provide products that represent all the ladies out there going after their dreams, 
+struggling to balance their lives, who achieved their goals but missing a piece, appreciate 
+them every single day because they are Queens, warriors, yekeberu, as we call them “KIBIRT
+     
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+   
+  )
+}
